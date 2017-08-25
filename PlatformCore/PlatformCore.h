6f3f2c3e2b482724486d6b4d4ef56020237cd772
keranmarinov <keran.marinov@constant.co>
2017-08-25 11:30:15 +0300

    Initial Commit

---
+++ PlatformCore/PlatformCore.h
@@ -0,0 +1,19 @@
+//
+//  PlatformCore.h
+//  PlatformCore
+//
+//  Created by linxmap on 25/08/2017.
+//  Copyright © 2017 luvio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlatformCore.
+FOUNDATION_EXPORT double PlatformCoreVersionNumber;
+
+//! Project version string for PlatformCore.
+FOUNDATION_EXPORT const unsigned char PlatformCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatformCore/PublicHeader.h>
+
+
